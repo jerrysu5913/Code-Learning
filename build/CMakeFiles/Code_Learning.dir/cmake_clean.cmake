6ea3e940cdf314841acf9ae95f1303f9351ed4f2
jerrysu5379 <48144074+jerrysu5379@users.noreply.github.com>
2023-06-06 15:02:18 +0000

    update

---
+++ build/CMakeFiles/Code_Learning.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Code_Learning.dir/main.c.o"
+  "CMakeFiles/Code_Learning.dir/main.c.o.d"
+  "Code_Learning"
+  "Code_Learning.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Code_Learning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Code_Learning.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Code_Learning.
+# This may be replaced when dependencies are built.
