3dfa1bafb41e2c5c4f3df3ef722c23b3efa96187
jerrysu5379 <48144074+jerrysu5379@users.noreply.github.com>
2023-06-11 08:31:19 +0000

    buggy……

---
+++ 期末考试模拟/(buggy)7-2 最小公倍数.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long long gcd(long long a, long long b){
+    if(b==0) return a;
+    return gcd(b, a%b);
+}
+
+long long lcm(long long a, long long b){
+    if(a==0 || b==0) return 0;
+    return llabs((a*b)/gcd(a,b));
+}
+
+int main(){
+    long long a,b;
+    scanf("%lld %lld",&a,&b);
+    printf("%lld",lcm(a,b));
+    return 0;
+}
