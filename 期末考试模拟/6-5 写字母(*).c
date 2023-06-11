726d9262238cc73724a19fe33c0910fc354b89c9
jerrysu5379 <48144074+jerrysu5379@users.noreply.github.com>
2023-06-11 12:00:54 +0000

    finished!

---
+++ 期末考试模拟/6-5 写字母(*).c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void WriteLetter(FILE *f, int n);
+
+int main()
+{
+    FILE *f;
+    int n;
+    f = fopen("Letter.txt", "w");
+    if (!f)
+    {
+        puts("文件无法打开!");
+        exit(1);
+    }
+
+    scanf("%d", &n);
+    WriteLetter(f, n);
+
+    if (fclose(f))
+    {
+        puts("文件无法关闭!");
+        exit(1);
+    }
+    puts("文件保存成功!");
+    return 0;
+}
+
+/* 你提交的代码将被嵌在这里 */
+void WriteLetter(FILE *f, int n){
+    for(int i=0;i<n;i++){
+        fputc('A'+i,f);
+    }
+}
